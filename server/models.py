b31f5460f9273d209bcd670e19bd844834b46de4
David N <paperish@icloud.com>
2025-12-18 21:20:37 -0700

    finished lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,21 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Author must have a name')
+        
+        if db.session.query(Author).filter_by(name=name).first():
+            raise ValueError("An author with this name already exists")
+        
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and phone_number.isdigit() and len(phone_number) == 10:
+            return phone_number
+        raise ValueError('Number must be exactly 10 digits')
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +43,30 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
-
+    @validates('content')
+    def validate_content(self, key, content):
+        if not content or len(content) < 250:
+            raise ValueError('Content must be at least 250 characters long')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError('Summary must be less than 250 characters.')
+        return summary
+        
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Must be either 'Fiction' or 'Non-Fiction'.")
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Post title must contain one of: 'Won't Believe', 'Secret', 'Top', or 'Guess'")
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
